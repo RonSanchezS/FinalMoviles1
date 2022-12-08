96c1b34cc6177bd27025a5a2320c591722632646
RonSanchezS <58291957+RonSanchezS@users.noreply.github.com>
2022-12-07 23:19:02 -0400

    Ahora la aplicacion puede buscar categorias por nombre

--- app/src/main/java/com/moviles/proyectomoviles/ActivityListaCategorias.kt
+++ app/src/main/java/com/moviles/proyectomoviles/ActivityListaCategorias.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -16,8 +18,12 @@ import com.moviles.proyectomoviles.repository.CotizacionRepository
 class ActivityListaCategorias : AppCompatActivity(), CategoryRepository.OnGetCategoriesListener,
     CategoryAdapter.onCategoryClickListener, CotizacionRepository.CotizacionListener {
     private lateinit var lstCategorias: RecyclerView
-    private lateinit var btn: Button
+    private lateinit var btnBusqueda: ImageButton
     private lateinit var btnVerCotizaciones: Button
+    private lateinit var inputPorBuscar: TextView
+
+    private lateinit var listaCategorias: List<Categories>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantalla_principal)
@@ -28,10 +34,19 @@ class ActivityListaCategorias : AppCompatActivity(), CategoryRepository.OnGetCat
     }
 
     private fun setUpListeners() {
-    btnVerCotizaciones.setOnClickListener {
-        val intent = Intent(this, ActivityCotizar::class.java)
-        startActivity(intent)
-    }
+        btnVerCotizaciones.setOnClickListener {
+            val intent = Intent(this, ActivityCotizar::class.java)
+            startActivity(intent)
+        }
+        btnBusqueda.setOnClickListener {
+         val textoCategoria =  inputPorBuscar.text.toString()
+            //filter categorias by textoCategoria
+            val categoriasFiltradas = listaCategorias.filter { it.name.contains(textoCategoria) }
+            val adapter = CategoryAdapter(categoriasFiltradas as ArrayList<Categories>, this)
+            lstCategorias.layoutManager = LinearLayoutManager(this)
+            lstCategorias.adapter = adapter
+
+        }
 
     }
 
@@ -41,17 +56,18 @@ class ActivityListaCategorias : AppCompatActivity(), CategoryRepository.OnGetCat
 
     private fun setUpListView() {
         lstCategorias = findViewById(R.id.lstCategories)
-        btn = findViewById(R.id.btn)
+        btnBusqueda = findViewById(R.id.btnBuscar)
         btnVerCotizaciones = findViewById(R.id.btnVerCotizaciones)
+        inputPorBuscar = findViewById(R.id.inputPorBuscar)
     }
 
     override fun onGetCategories(categories: List<Categories>?) {
+        listaCategorias = categories!!
         val adapter = CategoryAdapter(categories as ArrayList<Categories>, this)
         lstCategorias.layoutManager =
             LinearLayoutManager(this@ActivityListaCategorias)
         lstCategorias.adapter = adapter
     }
-
 
 
     override fun onGetCategoriesError(t: Throwable) {
@@ -66,14 +82,15 @@ class ActivityListaCategorias : AppCompatActivity(), CategoryRepository.OnGetCat
 
     override fun onCategoryClick(category: Categories) {
 
-            val intent = Intent(this, TrabajadoresDeCategoria::class.java)
+        val intent = Intent(this, TrabajadoresDeCategoria::class.java)
         intent.putExtra("categoryID", category.id)
         intent.putExtra("categoryName", category.name)
         startActivity(intent)
     }
 
     override fun onFailure(t: Throwable) {
-        Toast.makeText(this@ActivityListaCategorias, "No se pudo sacar datos", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this@ActivityListaCategorias, "No se pudo sacar datos", Toast.LENGTH_SHORT)
+            .show()
     }
 
     override fun onSuccess(cotizaciones: List<Cotizacion>?) {
